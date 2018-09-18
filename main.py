94171d81ce2ef2687f798c09abce62b1c72ae90c
seleonel <leon_bellini@outlook.com>
2018-09-18 20:00:01 -0300

    Funcoes para o funcionamento independente do programa criadas, funcao para debito terminada, consertados alguns erros de input

--- main.py
+++ main.py
@@ -4,9 +4,13 @@
 # ###############################################################
 # biblioteca os para a remoção do arquivo
 import os
+from datetime import datetime
+def pedirDinheiro():
+    return float(input("Digite um valor em reais: "))
 
 def checarCPF():
-    # numero de cpf é checado inumeras vezes
+    # numero de cpf é chamado muitas vezes no programa
+    # por isso, é melhor criar uma função
     while True:
         cpf = input("Agora digite seu CPF: ")
         if cpf == "" or int(cpf) <= 0:
@@ -15,11 +19,106 @@ def checarCPF():
             break
     return cpf
 
+def checarConta(cpf):
+    # maneira de checar se arquivo existe, logo, também checa o cpf
+    # retorna True se arquivo existe
+    return os.path.isfile("usuarios/%s.txt" % cpf)
 
-# funcao que retorna o arquivo para a conta para o cliente
+
+def checarSenha(arquivo):
+    # senha também importante checar
+    tentativas = 0
+    # usuario tem 3 tentativas para acertar a senha
+    while tentativas < 3:
+        senha_user = input("Por favor digite sua senha: ")
+        # leio a linha 4, onde toda senha está incluida
+        for iteracao, linhas in enumerate(arquivo):
+            if iteracao == 3:
+                # descarto o caractere de nova linha da senha
+                linha_limpa = linhas.strip("\n")
+                if linha_limpa == senha_user:
+                    return True
+            elif iteracao > 3:
+                break
+        print("Tente novamente")
+        tentativas += 1
+    else:
+        print("3 tentativas foram estouradas")
+
+
+
+def operacaoDebito(cpf):
+        arquivo_user = lerArq(cpf)
+        hist_user = lerHist(cpf)
+        itens = arquivo_user.readlines()
+        valores_hist = []
+        for linhas in hist_user:
+            valores_hist.append(linhas.split(" "))
+        hist_user.close()
+        valor_total = float(valores_hist[-1][-1])
+        valor_pedido = pedirDinheiro()
+        hist_user = modificarHist(cpf)
+        if itens[2].strip("\n") == "salario" or itens[2] == "salário":
+            # taxa de 5%
+            taxa_deb = valor_pedido * 0.05
+            novo_total = valor_total - (valor_pedido * 1.05) # taxas
+            if novo_total < 0:
+                print("Contas salário não podem ter saldo negativo")
+            else:
+                hist_user.write("%s %s %s %s %s\n" % (pegarTempo(), "-", valor_pedido, taxa_deb, novo_total))
+
+        elif itens[2].strip("\n") == "comum":
+            # taxa de 3%
+            taxa_deb = valor_pedido * 0.03
+            novo_total = valor_total - (valor_pedido * 1.03)
+            if novo_total < -500:
+                print("Contas comum não podem ter saldo negativo menor que R$ 500")
+            else:
+                hist_user.write("%s %s %s %s %s\n" % (pegarTempo(), "-", valor_pedido, taxa_deb, novo_total))
+
+
+        elif itens[2].strip("\n") == "plus":
+            # taxa de 1%
+            taxa_deb = valor_pedido * 0.01
+            novo_total = valor_total - (valor_pedido * 1.01)
+            if novo_total < -5000:
+                print("Contas plus não podem ter saldo negativo menor que R$ 5000")
+            else:
+                hist_user.write("%s %s %s %s %s\n" % (pegarTempo(), "-", valor_pedido, taxa_deb, novo_total))
+
+
+        else:
+            print("Sua conta foi criada incorretamente")
+
+
+        arquivo_user.close()
+        hist_user.close()
+        print("Débito realizado com sucesso! ")
+
+# funcoes que retornam o arquivo para a conta para o cliente nos modos
+# escrita, modificação e leitura, podem haver problemas de permissões em sistemas
+# como gnu/linux, não testado em mac
+
 def escreverArq(cpf):
     return open("usuarios/%s.txt" % cpf, "w")
 
+def modificarArq(cpf):
+    return open("usuarios/%s.txt" % cpf, "a")
+
+def lerArq(cpf):
+    return open("usuarios/%s.txt" % cpf, "r")
+# funcoes para leitura, escrita e alteração de historicos
+def escreverHist(cpf):
+    return open("historico/historico_%s.txt" % cpf, "w")
+
+def modificarHist(cpf):
+    return open("historico/historico_%s.txt" % cpf, "a")
+
+def lerHist(cpf):
+    return open("historico/historico_%s.txt" % cpf, "r")
+# funcao para retornar o tempo
+def pegarTempo():
+    return datetime.now().strftime('%Y-%m-%d %H:%M')
 
 def menuPrincipal():
     # 3 aspas funciona como strings de multilinhas, aqui posso "desenhar" um menu mais
@@ -52,7 +151,7 @@ def novoCliente():
     # criado um arquivo unico para cada usuario
     # caso haja falha num arquivo, nem todos os dados seriam perdidos
     arquivo_user = escreverArq(cpf_usuario)
-
+    historico_user = escreverHist(cpf_usuario)
     while True:
         tipo_conta = input("Digite o tipo de sua conta (salário, comum ou plus?): ")
         if tipo_conta == "salário" or tipo_conta == "salario" or tipo_conta == "comum" or tipo_conta == "plus":
@@ -73,10 +172,16 @@ def novoCliente():
             print("Por favor, digite uma senha válida")
         else:
             break
-    # A partir de agora, vou apenas trabalhar com essa ordem,
+    valor_total = valor_inicial
+    # A partir de agora, vou apenas trabalhar com essa ordem de itens
+    arquivo_user.write("%s\n%s\n%s\n%s\n" % (cpf_usuario, nome_usuario, tipo_conta, senha_usuario))
+    if valor_inicial == "0":
+        historico_user.write("%s %s %s 0 %s\n" % (pegarTempo(), "", valor_inicial, valor_total)) # Valor inicial nulo como primeiro "débito"
+    else:
+        historico_user.write("%s %s %s 0 %s\n" % (pegarTempo(), "+", valor_inicial, valor_total))
 
-    arquivo_user.write("%s\n%s\n%s\n%s\n%s\n" % (cpf_usuario, nome_usuario, tipo_conta, valor_inicial, senha_usuario))
     arquivo_user.close()
+    historico_user.close()
     print("Parabéns! Sua conta foi criada com sucesso.")
 
 
@@ -87,6 +192,7 @@ def apagarCliente():
         cpf_usuario = checarCPF()
         # funcao os remove apaga o arquivo
         os.remove("usuarios/%s.txt" % cpf_usuario)
+        os.remove("historico/historico_%s.txt" % cpf_usuario)
         print("Sua conta foi deletada com sucesso")
     else:
         # qualquer erro de digitação será desconsiderado para evitar acidentes
@@ -96,15 +202,41 @@ def apagarCliente():
 
 def contaDebito():
     cpf_usuario = checarCPF()
+    if checarConta(cpf_usuario):
+        arquivo_user = lerArq(cpf_usuario);
+        if checarSenha(arquivo_user):
+            arquivo_user.close()
+            operacaoDebito(cpf_usuario);
+    else:
+        print("Sua conta não existe ou não foi digitada corretamente")
 
 def contaDeposito():
     cpf_usuario = checarCPF()
+    if checarConta(cpf_usuario):
+        operacaoDeposito(cpf_usuario);
+    else:
+        print("Sua conta não existe ou não foi digitada corretamente")
+
 
 def contaSaldo():
     cpf_usuario = checarCPF()
+    if checarConta(cpf_usuario):
+        arquivo_user = lerArq(cpf_usuario);
+        if checarSenha(arquivo_user):
+            arquivo_user.close()
+            operacaoDeposito(cpf_usuario);
+    else:
+        print("Sua conta não existe ou não foi digitada corretamente")
 
 def contaExtrato():
     cpf_usuario = checarCPF()
+    if checarConta(cpf_usuario):
+        arquivo_user = lerArq(cpf_usuario);
+        if checarSenha(arquivo_user):
+            arquivo_user.close()
+            operacaoExtrato(cpf_usuario);
+    else:
+        print("Sua conta não existe ou não foi digitada corretamente")
 
 
 while True:
